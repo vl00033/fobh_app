5bb22ad554a363b1f50c78ac3ce626e59316f384
vl00033 <vl00033@surrey.ac.uk>
2012-01-11 19:00:31 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FobhApp::Application.config.secret_token = '2ffeafa2487452e01238a74549b5447d84022cdb69a84154ac8c341816575d8e37b59c5fc7504cdd3a521aeb2c8981abb3fc11266a401064e4dda580d42d5c88'
